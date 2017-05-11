718e07dcc7435659d2929bc98f6d95692af35c5a
Isaque Bezerra Vieira <bezerra.isaque@gmail.com>
2017-05-11 16:27:08 -0400

    PHP1

---
+++ PHP1/php1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP</title>
+    </head>
+    <body>
+        <?php
+            echo $_POST["nome"];
+            echo "<br>";
+            echo $_POST["email"];
+            echo "<br>";
+            echo $_POST["url"];
+            echo "<br>";
+            echo $_POST["mensagem"];
+        ?>
+    </body>
+</html>
