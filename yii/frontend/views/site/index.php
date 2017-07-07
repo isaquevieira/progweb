f1ea6589cbbc132f652a324b0491e75e3e5cf2e0
Isaque Bezerra Vieira <bezerra.isaque@gmail.com>
2017-07-06 23:23:00 -0400

    Adição da imagem do icomp na página principal

--- yii/frontend/views/site/index.php
+++ yii/frontend/views/site/index.php
@@ -1,20 +1,24 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Instituto de Computação';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <?= Html::img('@web/icomp.png',['width'=>'400']) ?>
+        <?= Html::a('Home', ['site/index']) ?>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p>O Instituto de Computação (IComp), antigo Departamento de Ciência da Computação (DCC),
+              é um instituto acadêmico que agrega os professores da área de computação.
+               Como todo instituto acadêmico o IComp atua no ensino, pesquisa e extensão,
+                além de desempenhar atividades administrativas.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
-    <div class="body-content">
+    <!-- <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
@@ -49,5 +53,5 @@ $this->title = 'My Yii Application';
             </div>
         </div>
 
-    </div>
+    </div> -->
 </div>
